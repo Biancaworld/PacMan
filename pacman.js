9e6858364516ee17516e7fb28bf18367343ba3b1
Biancaworld <113005568+Biancaworld@users.noreply.github.com>
2022-09-30 22:51:38 -0600

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -4,8 +4,8 @@ var pos = 0;
 let pageWidth = window.innerWidth;
 //This array contains all the PacMan movement images
 const pacArray = [
-  ['./PacMan1.png', './PacMan2.png'],
-  ['./PacMan3.png', './PacMan4.png'],
+  ['https://user-images.githubusercontent.com/113005568/193392871-55305fc3-5727-4d91-a890-a589deb848f1.png', 'https://user-images.githubusercontent.com/113005568/193392873-b83720a5-5455-4b70-9e00-afbd14693497.png'],
+  ['https://user-images.githubusercontent.com/113005568/193392874-5096481d-ebfb-4f9f-9463-8ab089f08be3.png', 'https://user-images.githubusercontent.com/113005568/193392875-67440857-3768-4e6c-8b68-262c5b995acc.png'],
 ];
 
 // this variable defines what direction should PacMan go into:
